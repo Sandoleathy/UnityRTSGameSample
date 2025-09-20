93995ec13d28bd8c3ac507d20970f82b045da6e0
Sandoleathy <3296446003@qq.com>
2025-09-20 23:55:49 +0800

    文件目录元数据和第三方模型文件

---
+++ Assets/Scripts/RTSUnits/TestUnit.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(OutlineObject))]
+public class TestUnit : RTSUnit
+{
+    private OutlineObject outline;
+    void Start()
+    {
+        SetMoveAlgorithm(new NavMeshMoveAlgorithm(GetComponent<NavMeshAgent>()));
+        SetAlertAlgorithm(new RangeAlertAlgorithm());
+    }
+
+    public override void OnSelected()
+    {
+        base.OnSelected();
+
+        if (outline == null)
+        {
+            outline = gameObject.AddComponent<OutlineObject>();
+        }
+
+        outline.enabled = true;
+    }
+
+    public override void DeSelected()
+    {
+        base.DeSelected();
+        if (outline != null)
+        {
+            outline.enabled = false;
+        }
+    }
+}
