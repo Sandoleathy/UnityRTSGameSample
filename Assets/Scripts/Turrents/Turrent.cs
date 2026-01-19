37942b72acb1eb27e951f1034406ac3865513491
Sandoleathy <3296446003@qq.com>
2026-01-19 12:17:19 +0800

    创建了炮塔类与默认配置文件，增加了加农炮武器配置

---
+++ Assets/Scripts/Turrents/Turrent.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Turrent : MonoBehaviour
+{
+    public TurrentConfig config;
+
+    private void Start() {
+        
+    }
+
+    private void RotateTowards(Vector3 targetPosition)
+    {
+        Vector3 direction = targetPosition - transform.position;
+        direction.y = 0f;
+
+        if (direction.sqrMagnitude < 0.0001f)
+            return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation,
+            targetRotation,
+            config.maxRotateSpeed * Time.deltaTime
+        );
+}
+
+    private bool IsAlignWithTarget(Vector3 targetPosition, float angleThreshold = 5f)
+    {
+        Vector3 directionToTarget = (targetPosition - transform.position).normalized;
+        float angle = Vector3.Angle(transform.forward, directionToTarget);
+        return angle <= angleThreshold;
+    }
+
+    public bool AimAndAttack(RTSUnit enemy)
+    {
+        if(IsAlignWithTarget(enemy.transform.position))
+        {
+            if (config.weapon != null)
+            {
+                return config.weapon.Attack(enemy);
+            }
+            else return false;
+        }
+        else
+        {
+            RotateTowards(enemy.transform.position);
+            return false;
+        }
+    }
+}
