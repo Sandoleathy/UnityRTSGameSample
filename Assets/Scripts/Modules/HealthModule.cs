f3b31a88878010822890da73a6176ac8dbb861e9
Sandoleathy <3296446003@qq.com>
2026-01-20 17:46:04 +0800

    拆分出的RTSUnit所需组件

---
+++ Assets/Scripts/Modules/HealthModule.cs
@@ -0,0 +1,61 @@
+
+using UnityEngine;
+
+public class HealthModule : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+    private RTSUnit owner;
+    public bool isAlive;
+    public void Init(RTSUnit owner)
+    {
+        currentHealth = maxHealth;
+        this.owner = owner;
+        isAlive = true;
+    }
+
+    public void OnDestroy()
+    {
+        isAlive = false;
+        return;
+    }
+
+    public void OnTakeDamage(float amount)
+    {
+        //当amount为负数时，表示治疗
+        currentHealth = Mathf.Min(currentHealth - amount, maxHealth);
+        if (currentHealth <= 0)
+        {
+            OnDead();
+        }
+    }
+
+    /// <summary>
+    /// 死亡时触发
+    /// </summary>
+    public void OnDead()
+    {
+        isAlive = false;
+        gameObject.layer = LayerMask.NameToLayer("DeadLayer");
+        //TODO: 死亡效果
+        Debug.Log($"{name} HP耗尽, 死亡了!");
+        Destroy(gameObject, 2f);
+        //临时留下调用死亡特效
+        owner.OnDead();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
